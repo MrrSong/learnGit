ee239f2d9dfe1b0de476280f9b37cbfb00a095dc
MrrSong <131602847+MrrSong@users.noreply.github.com>
2025-05-26 21:19:56 +0800

    Update setup_zsh.sh
    haoomz.zsh-theme

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install zsh git curl wget -y
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://install.ohmyz.sh)"
-wget --no-check-certificate -O $ZSH_CUSTOM/themes/haoomz.zsh-theme https://cdn.haoyep.com/gh/leegical/Blog_img/zsh/haoomz.zsh-theme
+sudo wget -O $ZSH_CUSTOM/themes/haoomz.zsh-theme https://cdn.haoyep.com/gh/leegical/Blog_img/zsh/haoomz.zsh-theme
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sed -i 's/ZSH_THEME=".*"/ZSH_THEME="haoomz"/' ~/.zshrc
